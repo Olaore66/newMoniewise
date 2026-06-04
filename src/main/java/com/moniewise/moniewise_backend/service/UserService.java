45d85f41106fd071c58969bf9bc4ef0d0934e2a1
olaore66 <olaore66@gmail.com>
2026-06-04 16:27:08 +0100

    newTransferFix17

--- src/main/java/com/moniewise/moniewise_backend/service/UserService.java
+++ src/main/java/com/moniewise/moniewise_backend/service/UserService.java
@@ -1,4 +1,4 @@
-﻿package com.moniewise.moniewise_backend.service;
+package com.moniewise.moniewise_backend.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
