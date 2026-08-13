32acce50497f29d174b807174b37c444b948e1d6
olaore66 <olaore66@gmail.com>
2026-08-13 13:58:01 +0100

    new_performance_issue_fix17

--- src/main/java/com/moniewise/moniewise_backend/config/AppConfig.java
+++ src/main/java/com/moniewise/moniewise_backend/config/AppConfig.java
@@ -1,21 +1,29 @@
 package com.moniewise.moniewise_backend.config;
 
-import com.moniewise.moniewise_backend.service.NotificationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.concurrent.Executor;
+
 @Configuration
-public class AppConfig {
+public class AppConfig implements AsyncConfigurer {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public com.moniewise.moniewise_backend.thirdParty.NotificationService notificationService() {
-//        return new NotificationService();
-//        // TODO: Replace with real service (e.g., Twilio for SMS, WebSocket for in-app) in future
-//    }
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
+    }
 }
