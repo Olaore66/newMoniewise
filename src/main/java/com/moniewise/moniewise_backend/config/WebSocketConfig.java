5fb33e36c5fb3b6ff967caed4c36f9f1b50b580f
olaore66 <olaore66@gmail.com>
2026-08-29 23:27:11 +0100

    new_ios_pushnotification_fix42

--- src/main/java/com/moniewise/moniewise_backend/config/WebSocketConfig.java
+++ src/main/java/com/moniewise/moniewise_backend/config/WebSocketConfig.java
@@ -12,9 +12,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketAuthChannelInterceptor webSocketAuthChannelInterceptor;
+    private final QuietWebSocketHandshakeHandler quietWebSocketHandshakeHandler;
 
-    public WebSocketConfig(WebSocketAuthChannelInterceptor webSocketAuthChannelInterceptor) {
+    public WebSocketConfig(WebSocketAuthChannelInterceptor webSocketAuthChannelInterceptor,
+                           QuietWebSocketHandshakeHandler quietWebSocketHandshakeHandler) {
         this.webSocketAuthChannelInterceptor = webSocketAuthChannelInterceptor;
+        this.quietWebSocketHandshakeHandler = quietWebSocketHandshakeHandler;
     }
 
     @Override
@@ -27,8 +30,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setHandshakeHandler(quietWebSocketHandshakeHandler)
                 .setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws-sockjs")
+                .setHandshakeHandler(quietWebSocketHandshakeHandler)
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
